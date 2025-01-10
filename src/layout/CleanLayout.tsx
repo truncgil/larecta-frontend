1a326fe98774d9067f6f3919994af6379eb9122e
Ümit Tunç <umitreva@gmail.com>
2025-01-10 13:04:23 +0300

    first commit

---
+++ src/layout/CleanLayout.tsx
@@ -0,0 +1,23 @@
+import React, { ReactNode } from 'react';
+
+const CleanLayout: React.FC<{ children: ReactNode }> = ({ children }) => {
+  return (
+    <div className="dark:bg-boxdark-2 dark:text-bodydark">
+      {/* <!-- ===== Page Wrapper Start ===== --> */}
+      <div className="flex h-screen overflow-hidden">
+        {/* <!-- ===== Main Content Start ===== --> */}
+        <div className="relative flex flex-1 flex-col overflow-y-auto overflow-x-hidden">
+          <main>
+            <div className="mx-auto max-w-screen-2xl p-4 md:p-6 2xl:p-10">
+              {children}
+            </div>
+          </main>
+          {/* <!-- ===== Main Content End ===== --> */}
+        </div>
+      </div>
+      {/* <!-- ===== Page Wrapper End ===== --> */}
+    </div>
+  );
+};
+
+export default CleanLayout; 
